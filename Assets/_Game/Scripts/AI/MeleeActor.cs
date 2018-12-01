82d0d157535c28acc9dc70f10cba926ecfa2eb7f
Manuel Buß <manuel.buss@gmx.de>
2018-12-01 23:42:00 +0100

    Removed Meshrender from melee

--- Assets/_Game/Scripts/AI/MeleeActor.cs
+++ Assets/_Game/Scripts/AI/MeleeActor.cs
@@ -10,22 +10,18 @@ public class MeleeActor : StupidActor
 
     protected override void Attack(GameObject target)
     {
-        renderer.material.color = Color.green;
         ExecuteEvents.ExecuteHierarchy<IHitTarget>(target, null, (x, y) => x.Damage(damage));
     }
 
     protected override void DamageTaken(int damage)
     {
-        renderer.material.color = Color.black;
     }
 
     protected override void Move()
     {
-        renderer.material.color = Color.blue;
     }
 
     protected override void Idle()
     {
-        renderer.material.color = Color.yellow;
     }
 }
