46fac1f83f6f484652f030b605b826dfb4e8f0e0
Markus Schlösser <schloem@fh-trier.de>
2018-12-01 20:34:32 +0100

    Added melee attack effect

--- Assets/_Game/Scripts/Character/MeleeAttackComponent.cs
+++ Assets/_Game/Scripts/Character/MeleeAttackComponent.cs
@@ -8,19 +8,38 @@ public class MeleeAttackComponent : MonoBehaviour {
 	public float attackSpeed;
 	public int damage;
 
+	public GameObject hitEffect;
+
 	private float timer;
 
 	private void Update() {
 		timer += Time.deltaTime;
 
 		if (timer >= 1.0f / attackSpeed) {
-			for(int i = 0; i < targets.Count; ++i) {
-				if ((targets[i].transform.position - transform.position).sqrMagnitude > range * range) continue;
+			timer -= 1.0f / attackSpeed;
+
+			float nearestSqrDistance = float.PositiveInfinity;
+			GameObject nearestTarget = null;
+
+			for (int i = 0; i < targets.Count; ++i) {
+				float sqrDistance = (targets[i].transform.position - transform.position).sqrMagnitude;
+				if (sqrDistance > range * range) continue;
 				if (Vector3.Dot(targets[i].transform.position - transform.position, transform.forward) < 0) continue;
+
+				if (sqrDistance < nearestSqrDistance) {
+					nearestSqrDistance = sqrDistance;
+					nearestTarget = targets[i];
+				}
+
 				ExecuteEvents.Execute<IHitTarget>(targets[i], null, (x, y) => x.Damage(damage));
 			}
 
-			timer -= 1.0f / attackSpeed;
+			if (nearestTarget != null) {
+				GameObject effect = Instantiate(hitEffect, transform);
+				effect.transform.rotation = Quaternion.FromToRotation(transform.forward, nearestTarget.transform.position - transform.position);
+			} else {
+				timer = 1.0f / attackSpeed;
+			}
 		}
 	}
 }
