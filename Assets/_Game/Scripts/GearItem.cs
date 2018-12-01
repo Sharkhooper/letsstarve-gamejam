d295b106432a6290889f97bf89350ce1cca589a5
Sebastian Frey <fratsch@gmx.de>
2018-12-01 21:37:14 +0100

    added several food and gear items

---
+++ Assets/_Game/Scripts/GearItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class GearItem : ScriptableObject
+{
+    [SerializeField] private int damageValue;
+
+    [SerializeField] private int rangeValue;
+
+    [SerializeField] private bool isRangedValue;
+
+    public int DamageValue => damageValue;
+
+    public int RangeValue => rangeValue;
+
+    public bool IsRangedValue => isRangedValue;
+}
