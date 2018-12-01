c74059980290c91c09d8ef6d426aea497df66156
Markus Schlösser <schloem@fh-trier.de>
2018-12-01 16:28:11 +0100

    Added blood on damage taken

--- Assets/_Game/Scripts/Character/CharacterActor.cs
+++ Assets/_Game/Scripts/Character/CharacterActor.cs
@@ -1,3 +1,4 @@
+using NaughtyAttributes;
 using UnityAtoms;
 using UnityEngine;
 using UnityEngine.AI;
@@ -16,9 +17,19 @@ public class CharacterActor : MonoBehaviour {
     public FoodComponent foodComponent;
 	public NavMeshAgent navMeshComponent;
 
+	public GameObject bloodParticleSystem;
+
     void Start() {
         healthComponent = GetComponent<HealthComponent>();
         foodComponent = GetComponent<FoodComponent>();
 		navMeshComponent = GetComponent<NavMeshAgent>();
+
+		healthComponent.OnDamageTaken += _ => OnDamage();
+	}
+
+	[Button]
+	private void OnDamage() {
+		GameObject blood = Instantiate(bloodParticleSystem, transform);
+		Destroy(blood, 45.0f);
 	}
 }
