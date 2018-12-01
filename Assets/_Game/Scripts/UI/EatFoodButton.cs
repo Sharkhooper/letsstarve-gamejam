3647e4c5bcc74c7d8b9c8c0cd4839e7c7cd09b1c
Soraphis <soraphis@googlemail.com>
2018-12-01 14:59:12 +0100

    more fancy UI animations

--- Assets/_Game/Scripts/UI/EatFoodButton.cs
+++ Assets/_Game/Scripts/UI/EatFoodButton.cs
@@ -1,6 +1,8 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class EatFoodButton : MonoBehaviour, IPointerClickHandler {
 
@@ -8,22 +10,34 @@ public class EatFoodButton : MonoBehaviour, IPointerClickHandler {
     public FoodItem item;
     public TMP_Text itemCountLabel;
 
+    private Image background;
+    
     private void Start() {
         if (item == null) this.gameObject.SetActive(false);
+        background = GetComponent<Image>();
     }
 
     void Update() {
         itemCountLabel.text = ""+inventory.FoodItems[item];
+        background.color = PartyFoodUISegment.SelectedPartyFoodSegment == null ? Color.gray : Color.white;
+        
     }
     
     public void OnPointerClick(PointerEventData eventData) {
-        if(PartyFoodUISegment.SelectedPartyFoodSegment == null) return;
+        if (PartyFoodUISegment.SelectedPartyFoodSegment == null) {
             // alert: select someone to make him eat food \o/
-
-        int i = inventory.FoodItems[item];
+            return;
+        }
+        
         
         if(inventory.RemoveItem(item, 1))
             PartyFoodUISegment.SelectedPartyFoodSegment.partyMember.foodComponent.saturation += item.Value;
+            var seq = DOTween.Sequence();
+            seq.Append(transform.DOScale(0.9f, 0.1f).SetEase(Ease.InCubic));
+            seq.Append(transform.DOScale(1, 0.1f).SetEase(Ease.OutCubic));
+        else {
+            this.transform.DOShakePosition(0.1f, 1f);
+        }
 
     }
 }
