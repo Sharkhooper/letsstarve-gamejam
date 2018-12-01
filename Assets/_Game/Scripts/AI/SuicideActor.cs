f65b7875892fdfe5cf2d14d59e5a06994916855f
Manuel Buß <manuel.buss@gmx.de>
2018-12-01 12:48:15 +0100

    Added abstract class  StupidActor

--- Assets/_Game/Scripts/AI/SuicideActor.cs
+++ Assets/_Game/Scripts/AI/SuicideActor.cs
@@ -1,53 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class SuicideActor : MonoBehaviour
+public class SuicideActor : StupidActor
 {
-    [SerializeField] private int damage = 50;
-
-    private StupidBehaviour behaviour;
-    private MeshRenderer renderer;
-    private HealthComponent health;
-
-    private void Awake()
-    {
-        behaviour = GetComponent<StupidBehaviour>();
-        renderer = transform.GetComponentInChildren<MeshRenderer>();
-        health = transform.GetComponent<HealthComponent>();
-
-        health.OnDeath += Death;
-        health.OnDamageTaken += DamageTaken;
-
-        behaviour.OnAttack += Attack;
-        behaviour.OnMove += Move;
-        behaviour.OnWait += Idle;
-    }
-
-    private void Death(int damage)
+    protected override void Death(int damage)
     {
         Destroy(gameObject);
     }
 
-    private void Attack(GameObject target)
+    protected override void DamageTaken(int damage)
+    {
+        renderer.material.color = Color.black;
+    }
+
+    protected override void Attack(GameObject target)
     {
         renderer.material.color = Color.red;
         ExecuteEvents.ExecuteHierarchy<IHitTarget>(target, null, (x, y) => x.Damage(damage));
         Destroy(gameObject);
     }
 
-    private void DamageTaken(int damage)
-    {
-        renderer.material.color = Color.black;
-    }
-
-    private void Move()
+    protected override void Move()
     {
         renderer.material.color = Color.blue;
     }
 
-    private void Idle()
+    protected override void Idle()
     {
         renderer.material.color = Color.yellow;
     }
