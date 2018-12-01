7a7d6df48f3d04e4ebf76fb5a5e289b03e8c6e1f
Sebastian Frey <fratsch@gmx.de>
2018-12-02 00:10:16 +0100

    MeleeAttack differentiates between ranged and melee

--- Assets/_Game/Scripts/Character/MeleeAttackComponent.cs
+++ Assets/_Game/Scripts/Character/MeleeAttackComponent.cs
@@ -4,15 +4,25 @@ using UnityEngine.EventSystems;
 
 public class MeleeAttackComponent : MonoBehaviour {
 	public GameObjectList targets;
-	public float range;
+	
 	public float attackSpeed;
-	public int damage;
+
+	private int damage;
+    private float range;
+
+    public float rangedStat = 0.5f;
 
 	public GameObject hitEffect;
 
 	private float timer;
 
 	private CharacterActor actor;
+
+    private float nearestSqrDistance;
+    private float nearestUnavailableSqrDistance;
+
+    private GameObject nearestTarget;
+    private GameObject nearestUnavailableTarget;
 
 	private void Awake() {
 		actor = GetComponent<CharacterActor>();
@@ -24,35 +34,26 @@ public class MeleeAttackComponent : MonoBehaviour {
 		if (timer >= 1.0f / attackSpeed) {
 			timer -= 1.0f / attackSpeed;
 
-			float nearestSqrDistance = float.PositiveInfinity;
-			GameObject nearestTarget = null;
+			nearestSqrDistance = float.PositiveInfinity;
+			nearestTarget = null;
 
-			float nearestUnavailableSqrDistance = float.PositiveInfinity;
-			GameObject nearestUnavailableTarget = null;
+			nearestUnavailableSqrDistance = float.PositiveInfinity;
+			nearestUnavailableTarget = null;
 
-			for (int i = 0; i < targets.Count; ++i) {
-				float sqrDistance = (targets[i].transform.position - transform.position).sqrMagnitude;
-				if (sqrDistance > range * range) continue;
-				if (Vector3.Dot(targets[i].transform.position - transform.position, actor.Forward) < 0) {
-					if (sqrDistance < nearestUnavailableSqrDistance) {
-						nearestUnavailableSqrDistance = sqrDistance;
-						nearestUnavailableTarget = targets[i];
-					}
-					continue;
-				}
-
-				if (sqrDistance < nearestSqrDistance) {
-					nearestSqrDistance = sqrDistance;
-					nearestTarget = targets[i];
-				}
-
-				ExecuteEvents.Execute<IHitTarget>(targets[i], null, (x, y) => x.Damage(damage));
-			}
+		    if (actor.inventory.EquippedItems[actor].IsRangedValue)
+		    {
+                attackRanged();
+		    }
+		    else
+		    {
+		        attackMelee();
+		    }
 
 			if (nearestTarget != null) {
 				GameObject effect = Instantiate(hitEffect, transform);
 				effect.transform.rotation = Quaternion.FromToRotation(Vector3.forward, nearestTarget.transform.position - transform.position);
-			} else if (nearestUnavailableTarget != null) {
+			    actor.theta = -Vector3.SignedAngle(Vector3.right, nearestUnavailableTarget.transform.position - transform.position, Vector3.up);
+            } else if (nearestUnavailableTarget != null) {
 				actor.theta = -Vector3.SignedAngle(Vector3.right, nearestUnavailableTarget.transform.position - transform.position, Vector3.up);
 				timer = 1.0f / attackSpeed;
 			} else {
@@ -60,4 +61,60 @@ public class MeleeAttackComponent : MonoBehaviour {
 			}
 		}
 	}
+
+    void attackMelee()
+    {
+        for (int i = 0; i < targets.Count; ++i)
+        {
+            float sqrDistance = (targets[i].transform.position - transform.position).sqrMagnitude;
+            if (sqrDistance > range * range) continue;
+            if (Vector3.Dot(targets[i].transform.position - transform.position, actor.Forward) < 0)
+            {
+                if (sqrDistance < nearestUnavailableSqrDistance)
+                {
+                    nearestUnavailableSqrDistance = sqrDistance;
+                    nearestUnavailableTarget = targets[i];
+                }
+                continue;
+            }
+
+            if (sqrDistance < nearestSqrDistance)
+            {
+                nearestSqrDistance = sqrDistance;
+                nearestTarget = targets[i];
+            }
+
+            damage = (int)(actor.inventory.EquippedItems[actor].DamageValue * (1 + rangedStat));
+            ExecuteEvents.Execute<IHitTarget>(targets[i], null, (x, y) => x.Damage(damage));
+        }
+    }
+    void attackRanged()
+    {
+        for (int i = 0; i < targets.Count; ++i)
+        {
+            float sqrDistance = (targets[i].transform.position - transform.position).sqrMagnitude;
+            if (sqrDistance > range * range) continue;
+            if (Vector3.Dot(targets[i].transform.position - transform.position, actor.Forward) < 0)
+            {
+                if (sqrDistance < nearestUnavailableSqrDistance)
+                {
+                    nearestUnavailableSqrDistance = sqrDistance;
+                    nearestUnavailableTarget = targets[i];
+                }
+                continue;
+            }
+
+            if (sqrDistance < nearestSqrDistance)
+            {
+                nearestSqrDistance = sqrDistance;
+                nearestTarget = targets[i];
+            }
+
+            if (nearestTarget != null)
+            {
+                damage = (int)(actor.inventory.EquippedItems[actor].DamageValue * (1 + rangedStat));
+                ExecuteEvents.Execute<IHitTarget>(targets[i], null, (x, y) => x.Damage(damage));
+            }
+        }
+    }
 }
