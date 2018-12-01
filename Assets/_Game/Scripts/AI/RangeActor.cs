53f60e0d77140a2ba1d4e351bcd2c9a04f723ebc
Manuel Buß <manuel.buss@gmx.de>
2018-12-01 17:38:06 +0100

    Created Melee and Ranged Ai prefabs

---
+++ Assets/_Game/Scripts/AI/RangeActor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RangeActor : StupidActor
+{
+    protected override void Death(int damage)
+    {
+        Destroy(gameObject);
+    }
+
+    protected override void Attack(GameObject target)
+    {
+        renderer.material.color = Color.magenta;
+        ExecuteEvents.ExecuteHierarchy<IHitTarget>(target, null, (x, y) => x.Damage(damage));
+    }
+
+    protected override void DamageTaken(int damage)
+    {
+        renderer.material.color = Color.black;
+    }
+
+    protected override void Move()
+    {
+        renderer.material.color = Color.blue;
+    }
+
+    protected override void Idle()
+    {
+        renderer.material.color = Color.yellow;
+    }
+}
