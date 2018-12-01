d8c8ed3f4d199ef9b24e0929d471dea065df4a5b
Sebastian <fratsch@gmx.de>
2018-12-01 18:37:27 +0100

    added pickup script

---
+++ Assets/_Game/Scripts/Pickupables/PickUp.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void OnTriggerEntered(Collider other)
+    {
+        this.gameObject.SetActive(false);
+    }
+}
