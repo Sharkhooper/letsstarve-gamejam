ff32c48b5ff98f776d0a9c10c9235bba1f119106
Sebastian Frey <fratsch@gmx.de>
2018-12-01 19:37:45 +0100

    item dummies can now be picked up and are deactivated

--- Assets/_Game/Scripts/Pickupables/PickUp.cs
+++ Assets/_Game/Scripts/Pickupables/PickUp.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PickUp : MonoBehaviour
 {
-    void OnTriggerEntered(Collider other)
+    void OnTriggerEnter(Collider other)
     {
+        Debug.Log(other);
         this.gameObject.SetActive(false);
     }
 }
