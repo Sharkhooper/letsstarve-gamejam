820c171a922de81731fb9ba4931c89797be813e6
Soraphis <soraphis@googlemail.com>
2018-12-01 10:57:40 +0100

    basic ui scenes first commit WIP

---
+++ Assets/_Game/Scripts/CharacterActor.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+
+
+public class CharacterActor : MonoBehaviour {
+
+    public float health, food; // TODO: replace with other components stuff ... 
+
+}
---
+++ Assets/_Game/Scripts/UI/PartyFoodUISegment.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityAtoms;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PartyFoodUISegment : MonoBehaviour {
+    private int idx = -1;
+    public GameObjectList partyList;
+    private CharacterActor partyMember;
+
+    [BoxGroup("UI Elements")] [SerializeField]
+    private Image characterIcon, healthBar, foodBar; 
+    
+    // Start is called before the first frame update
+    void Start() {
+        idx = this.transform.GetSiblingIndex();
+        partyMember = partyList[idx].GetComponent<CharacterActor>();
+        
+        // todo: characterIcon ... 
+    }
+
+    // Update is called once per frame
+    void Update() {
+        healthBar.fillAmount = partyMember.health;
+        foodBar.fillAmount = partyMember.food;
+    }
+}
